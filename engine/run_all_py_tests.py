4dc2d3e959f6168542ae5b4c54c4ab25d4b9c407
basicula <maksim.basum@gmail.com>
2019-12-19 22:03:07 +0200

    Update script for tests

--- engine/run_all_py_tests.py
+++ engine/run_all_py_tests.py
@@ -1,13 +1,23 @@
 import os
 import sys
 import subprocess
+import json
 
 def run(module=""):
     folder = os.walk(".")
+    short_results = []
     for addr, dirs, files in folder:
         for file in files:
             if "_test.py" in file and module in addr:
-                subprocess.run(["python3", addr+"/"+file])
-            
+                res = file + ": "
+                res += "OK" if subprocess.run(["python3", addr+"/"+file]).returncode == 0 else "Failed"
+                short_results.append(res)
+    
+    
+    print("-------Short results-------")
+    for res in short_results:
+        print(res)
+        
+
 if __name__ == "__main__":
     run(sys.argv[1])
