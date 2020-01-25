21545616464c66d9b9d328dbfc2ea446b056a79c
Maksym Bas <maksym.bas@materialise.kiev.ua>
2020-01-25 18:08:54 +0200

    Start SPHSystem

--- engine/Common/include/DefinesAndConstants.h
+++ engine/Common/include/DefinesAndConstants.h
@@ -3,14 +3,34 @@
 
 //-----------------------Math Common Constants-----------------------//
 
-#define Epsilon3D   1e-12
-#define PI          3.14159265359
-#define SQRT_2      1.41421356237
-#define SQRT_3      1.73205080757
+constexpr double Epsilon3D  = 1e-12;
+constexpr double PI         = 3.14159265359;
+constexpr double SQRT_2     = 1.41421356237;
+constexpr double SQRT_3     = 1.73205080757;
 
-static const double MAX_DOUBLE  = std::numeric_limits<double>::max(); // 1.79769e+308
-static const double MIN_DOUBLE  = std::numeric_limits<double>::min(); // 2.22507e-308
-static const int MAX_INT        = std::numeric_limits<int>::max(); // 2147483647
-static const int MIN_INT        = std::numeric_limits<int>::min(); // -2147483648
+// 1.79769e+308
+constexpr double MAX_DOUBLE  = std::numeric_limits<double>::max(); 
+
+// 2.22507e-308
+constexpr double MIN_DOUBLE  = std::numeric_limits<double>::min();
+
+// 2147483647
+constexpr int MAX_INT        = std::numeric_limits<int>::max(); 
+
+// -2147483648
+constexpr int MIN_INT        = std::numeric_limits<int>::min(); 
 
 //-----------------------Math Common Constants End-----------------------//
+
+//-----------------------Physics Common Constants---------------------------//
+
+// Length   L         meters          m
+// Volume   V                         m^3
+// Density  D/rho                     kg / m^3
+// Mass     M         kilograms       kg
+// Time     T         seconds         s
+// Force    F         newtons         kg * m / s^(-2)
+
+constexpr double WATER_DENSITY = 1000.0;
+
+//-----------------------Physics Common Constants End-----------------------//
--- engine/Fluid/include/SPHSystem.h
+++ engine/Fluid/include/SPHSystem.h
@@ -6,6 +6,27 @@ class SPHSystem : public ParticleSystem
   public:
     SPHSystem(std::size_t i_num_particles);
 
+    VectorDataIteratorC GetPositions() const;
+    VectorDataIterator GetPositions();
+
+    VectorDataIteratorC GetVelocities() const;
+    VectorDataIterator GetVelocities();
+
+    VectorDataIteratorC GetForces() const;
+    VectorDataIterator GetForces();
+
+    ScalarDataIteratorC GetDensities() const;
+    ScalarDataIterator GetDensities();
+
+    ScalarDataIteratorC GetPressures() const;
+    ScalarDataIterator GetPressures();
+
+    double GetRadius() const;
+    void SetRadius(double i_radius);
+
+    double GetMass() const;
+    void SetMass(double i_mass);
+
   private:
     std::size_t m_positions_id;
     std::size_t m_velocities_id;
@@ -13,4 +34,77 @@ class SPHSystem : public ParticleSystem
 
     std::size_t m_densities_id;
     std::size_t m_pressures_id;
+
+    double m_radius;
+    double m_mass;
   };
+
+inline ParticleSystem::VectorDataIteratorC SPHSystem::GetPositions() const
+  {
+  return _GetVectorDataAt(m_positions_id);
+  }
+
+inline ParticleSystem::VectorDataIterator  SPHSystem::GetPositions()
+  {
+  return _GetVectorDataAt(m_positions_id);
+  }
+
+inline ParticleSystem::VectorDataIteratorC SPHSystem::GetVelocities() const
+  {
+  return _GetVectorDataAt(m_velocities_id);
+  }
+
+inline ParticleSystem::VectorDataIterator  SPHSystem::GetVelocities()
+  {
+  return _GetVectorDataAt(m_velocities_id);
+  }
+
+inline ParticleSystem::VectorDataIteratorC SPHSystem::GetForces() const
+  {
+  return _GetVectorDataAt(m_forces_id);
+  }
+
+inline ParticleSystem::VectorDataIterator  SPHSystem::GetForces()
+  {
+  return _GetVectorDataAt(m_forces_id);
+  }
+
+inline ParticleSystem::ScalarDataIteratorC SPHSystem::GetDensities() const
+  {
+  return _GetScalarDataAt(m_densities_id);
+  }
+
+inline ParticleSystem::ScalarDataIterator  SPHSystem::GetDensities()
+  {
+  return _GetScalarDataAt(m_densities_id);
+  }
+
+inline ParticleSystem::ScalarDataIteratorC SPHSystem::GetPressures() const
+  {
+  return _GetScalarDataAt(m_pressures_id);
+  }
+
+inline ParticleSystem::ScalarDataIterator  SPHSystem::GetPressures()
+  {
+  return _GetScalarDataAt(m_pressures_id);
+  }
+
+inline double SPHSystem::GetMass() const
+  {
+  return m_mass;
+  }
+
+inline void SPHSystem::SetMass(double i_mass)
+  {
+  m_mass = i_mass;
+  }
+
+inline double SPHSystem::GetRadius() const
+  {
+  return m_radius;
+  }
+
+inline void SPHSystem::SetRadius(double i_radius)
+  {
+  m_radius = i_radius;
+  }
